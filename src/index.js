aed48032debb46ec18d1160f6116bd7d731992c3
Akshat Ramanathan <ramanathan.ak@northeastern.edu>
2024-01-14 12:02:55 -0500

    test function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-async function fetchQuestions(){
+async function fetchQuestions() {
     const response = await fetch('/questions.json');
     const questions = await response.json();
     // console.log(questions);
     return questions;
 }
 
-function displayQuestion(question){
+function cleanup() {
     //cleanup
     const questionsDiv = document.getElementById('questions');
     // questionsDiv.innerHTML="";
+}
+function displayQuestion(question) {
+    cleanup();
     //create block
     const newBlock = document.createElement('div');
-    newBlock.setAttribute("id",`question${question._id}`)
+    newBlock.setAttribute("id", `question${question._id}`)
     newBlock.innerHTML = `<h3>${question.question}</h3><span>Points: ${question.points}</span><table><tr><td>${question.options[0]}</td></tr><tr><td>${question.options[1]}</td></tr><tr><td>${question.options[2]}</td></tr><tr><td>${question.options[3]}</td></tr></table>`;
     questionsDiv.appendChild(newBlock);
 }
-async function start(){
+async function start() {
     //hide start button 
     document.getElementById("start").style.display = "none";
     //get questions
-    const QUESTIONS = await fetchQuestions();  
-    for(question of QUESTIONS){
-        displayQuestion(question);
-    }
+    const QUESTIONS = await fetchQuestions();
+    QUESTIONS.forEach(question => displayQuestion(question));
 
     //on next click, change question div
     //on end, display score and restart button
