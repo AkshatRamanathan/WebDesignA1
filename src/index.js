25a653076631a09e0c3baae740215bc01cd33004
AkshatRamanathan <ramanathan.ak@northeastern.edu>
2024-01-17 14:33:43 -0500

    style fixes and render fixes

--- src/index.js
+++ src/index.js
@@ -10,21 +10,41 @@ function cleanup() {
     const questionsDiv = document.getElementById('questions');
     // questionsDiv.innerHTML="";
 }
+
 function displayQuestion(question) {
     cleanup();
+    const questionsDiv = document.getElementById('questions');
     //create block
     const newBlock = document.createElement('div');
     newBlock.setAttribute("id", `question${question._id}`)
-    newBlock.innerHTML = `<h3>${question.question}</h3><span>Points: ${question.points}</span><table><tr><td>${question.options[0]}</td></tr><tr><td>${question.options[1]}</td></tr><tr><td>${question.options[2]}</td></tr><tr><td>${question.options[3]}</td></tr></table>`;
+    newBlock.innerHTML = `<h3>${question.question}</h3><span>Points: ${question.points}</span><table><tr><td><input type="radio" id="${question._id}" name="options${question._id}" value="${question.options[0].name}">${question.options[0].name}</td></tr><tr><td><input type="radio" id="${question._id}" name="options${question._id}" value="${question.options[1].name}">${question.options[1].name}</td></tr><tr><td><input type="radio" id="${question._id}" name="options${question._id}" value="${question.options[2].name}">${question.options[2].name}</td></tr><tr><td><input type="radio" id="${question._id}" name="options${question._id}" value="${question.options[3].name}">${question.options[3].name}</td></tr></table>`;
     questionsDiv.appendChild(newBlock);
 }
+
+function handleSubmit(){
+    document.getElementById('questions').innerHTML="";
+
+}
+
+function submitButtonAction(){
+    const questionsDiv = document.getElementById('questions');
+    const submitButton = document.createElement('button');
+    submitButton.textContent = "SUBMIT";
+    submitButton.setAttribute("id","submitButton");
+    submitButton.addEventListener('click',handleSubmit);
+    questionsDiv.appendChild(submitButton);
+}
+
 async function start() {
     //hide start button 
     document.getElementById("start").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("instructions").style.display = "none";
     //get questions
-    const QUESTIONS = await fetchQuestions();
+    QUESTIONS = await fetchQuestions();
     QUESTIONS.forEach(question => displayQuestion(question));
-
-    //on next click, change question div
-    //on end, display score and restart button
+    submitButtonAction();
 }
+let QUESTIONS = [];
+let SCORE = 0;
+let answers = [];
